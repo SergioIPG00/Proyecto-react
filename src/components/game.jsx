df5ddd5e46775fb5b97f18870d2b63310251381a
SergioIPG00 <ivansergio472@gmail.com>
2023-09-10 19:07:30 -0500

    prueba

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,6 +1,7 @@
 function Game({title, platform, genre, release_date, image}){
     return(
         <div className="card m-2" style={{width: "300px"}}>
+            
             <img src={image} className="card-img-top" alt=""/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>                
